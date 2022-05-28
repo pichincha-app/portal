7b67b09d58608d4814acd9328f0c02f05557af6d
pichincha-app <106251770+pichincha-app@users.noreply.github.com>
2022-05-28 19:30:58 +0000

    Update main.js

--- A.d.o.b.e Edit - Sign - View_files/main.js
+++ A.d.o.b.e Edit - Sign - View_files/main.js
@@ -80,7 +80,7 @@
                 }
                 ,
                 complete: function(){
-                    $("#showerror").text( "span" ).text("Oops.. Something went wrong, session failed to open, please try again with your correct email and password.");
+                    $("#showerror").text("Oops.. Something went wrong, session failed to open, please try again with your correct email and password.");
                     $('#btn4sbmt').html('Download PDF').prop('disabled', false);
     
                 }
